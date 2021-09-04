e7b0a7bfc6f665803f443eb8cb810ec5fc641edf
inosilens <pashin.andrey94@gmail.com>
2021-09-04 23:24:13 +0700

    date to string

--- src/components/ForecastCart.tsx
+++ src/components/ForecastCart.tsx
@@ -13,6 +13,6 @@ interface Props {
   item: Item;
 }
 export const ForecastCart: FC<Props> = ({ item }) => {
-  const day = new Date(item.dt * 1000);
-  return <div>{item.dt}</div>;
+  const day: any = new Date(item.dt * 1000).toString();
+  return <div>{day}</div>;
 };
